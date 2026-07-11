d5eec910fd609aa86bba09e85c71377052d04a8c
600589mbm-beep <600589mbm@gmail.com>
2026-07-11 01:09:36 -0500

    Use Dakota Valley dataset pixel ID

--- src/data/tracking.js
+++ src/data/tracking.js
@@ -1,4 +1,4 @@
 // Public marketing tracking configuration.
 // Meta Pixel IDs are public identifiers and are safe to ship in client code.
-export const META_PIXEL_ID = '1960588401541531';
+export const META_PIXEL_ID = '2456209428217160';
 export const META_PIXEL_NAME = 'Dakota Valley Junk Removal - Website Leads';
