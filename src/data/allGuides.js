b8564dd1b6d3d6a22210836952f718676fcbdd28
600589mbm-beep <600589mbm@gmail.com>
2026-07-24 01:38:13 -0500

    Include final local SEO guide group

--- src/data/allGuides.js
+++ src/data/allGuides.js
@@ -1,10 +1,12 @@
 import { guides as existingGuides } from './guides.js';
 import { seoGuides } from './seoGuides.js';
 import { seoGuidesRoundTwo } from './seoGuidesRoundTwo.js';
+import { seoGuidesFinal } from './seoGuidesFinal.js';
 
 export const guides = {
   ...seoGuides,
   ...seoGuidesRoundTwo,
+  ...seoGuidesFinal,
   ...existingGuides,
 };
 
