55540ac927a672757e5760566e5edd64ef9cd092
600589mbm-beep <600589mbm@gmail.com>
2026-07-24 01:22:42 -0500

    Include second local SEO guide group

--- src/data/allGuides.js
+++ src/data/allGuides.js
@@ -1,8 +1,10 @@
 import { guides as existingGuides } from './guides.js';
 import { seoGuides } from './seoGuides.js';
+import { seoGuidesRoundTwo } from './seoGuidesRoundTwo.js';
 
 export const guides = {
   ...seoGuides,
+  ...seoGuidesRoundTwo,
   ...existingGuides,
 };
 
